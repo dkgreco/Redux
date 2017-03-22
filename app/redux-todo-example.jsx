9f04ef2418b3a527d6504346ff2f1f3aea8d7b54
Dustin K Greco <dgreco.dev@gmail.com>
2017-03-22 16:24:36 -0600

    Add action generators, multiple reducers

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,30 +1,109 @@
 const redux = require('redux');
 console.log('Starting Redux');
 
-let defaultState = {
-    showCompleted: false,
-    searchFilter: '',
-    taskList:[]
+let testActionList = () => {
+    "use strict";
+
 };
 
-let reducer = (state, action) => {
+let dispatch = action => {
     "use strict";
-    state = state || defaultState;
+    return store.dispatch(action);
+};
+
+//SearchFilter Reducer and Action Generators
+//---------------
+let searchFilterReducer = (state, action) => {
+    "use strict";
+    state = state || 'Anonymous';
     switch(action.type) {
         case 'SET_FILTER':
-            return {
-                ...state,
-                searchFilter: action.searchFilter
-            };
-        case 'SHOW_COMPLETED':
-            return {
-                ...state,
-                showCompleted: action.showCompleted
-            };
+            return action.searchFilter;
         default:
             return state;
     }
 };
+
+let dispatchChangeToSearchFilter = searchFilter => {
+    "use strict";
+    return {
+        type: 'SET_FILTER',
+        searchFilter: searchFilter
+    }
+};
+
+let setSearchFilterTo = searchFilter => dispatch(dispatchChangeToSearchFilter(searchFilter));
+
+//Show all completed tasks Reducer and Action Generators
+//---------------
+let showCompletedReducer = (state, action) => {
+    "use strict";
+    state = state || false;
+    switch(action.type) {
+        case 'SHOW_COMPLETED':
+            return action.showCompleted;
+        default:
+            return state;
+    }
+};
+
+let dispatchChangeToShowCompletedTasks = boolVal => {
+    "use strict";
+    return {
+        type: 'SHOW_COMPLETED',
+        showCompleted: boolVal
+    }
+};
+
+let showCompletedTasks = boolVal => dispatch(dispatchChangeToShowCompletedTasks(boolVal));
+
+//taskList Reducer and Action Generators
+//---------------
+let nextTaskId = 1;
+let taskListReducer = (state, action) => {
+    "use strict";
+    state = state || [];
+    switch(action.type) {
+        case 'ADD_TASK':
+            return [
+                ...state,
+                {
+                    id: nextTaskId++,
+                    task: action.task
+                }
+            ];
+        case 'DEL_TASK':
+            return state.filter(task => task.id !== action.id);
+        default:
+            return state;
+    }
+};
+
+let dispatchAdditionToTaskList = task => {
+    "use strict";
+    return {
+        type: 'ADD_TASK',
+        task: task
+    }
+};
+
+let addTaskToList = task => dispatch(dispatchAdditionToTaskList(task));
+
+let dispatchSubtractionFromTaskList = taskId => {
+    "use strict";
+    return {
+        type: 'DEL_TASK',
+        id: taskId
+    }
+};
+
+let removeTaskFromList = taskId => dispatch(dispatchSubtractionFromTaskList(taskId));
+
+let reducer = redux.combineReducers({
+    searchFilter: searchFilterReducer,
+    showCompleted: showCompletedReducer,
+    taskList: taskListReducer
+});
 
 let store = redux.createStore(reducer, redux.compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -34,30 +113,12 @@ let store = redux.createStore(reducer, redux.compose(
 store.subscribe(() => {
     "use strict";
     let state = store.getState();
-    console.log('Search Text Change: ', state.searchFilter);
-    console.log('Show Completed Tasks: ', state.showCompleted);
+    console.log('State Change: ', state);
 });
 
-let currentState = store.getState();
-
-console.log('Current State: ', currentState);
-
 //all actions must be objects
-let action1 = {
-    type: 'SHOW_COMPLETED',
-    showCompleted: true
-};
-
-let action2 = {
-    type: 'SET_FILTER',
-    searchFilter: 'Feed Mister'
-};
-
-let action3 = {
-    type: 'SET_FILTER',
-    searchFilter: 'Make Dinner'
-};
-
-store.dispatch(action1);
-store.dispatch(action2);
-store.dispatch(action3);
+setSearchFilterTo('Mister');
+showCompletedTasks(true);
+addTaskToList('Feed Mister');
+addTaskToList('Walk Bailey');
+removeTaskFromList(1);
